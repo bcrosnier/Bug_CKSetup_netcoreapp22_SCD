d863fa2c3d0283ecc15ad98b991b461c96e1996b
Benjamin Crosnier <ben.crosnier@gmail.com>
2019-09-21 08:47:43 +0200

    Initial commit

---
+++ Bug_CKSetup_netcoreapp22_SCD/Database/Package.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CK.Setup;
+using CK.SqlServer.Setup;
+
+namespace Bug_CKSetup_netcoreapp22_SCD.Database
+{
+    [SqlPackage(Schema = "CK", ResourcePath = "Res")]
+    [Versions("1.0.0")]
+    public abstract class Package : SqlPackage
+    {
+        void StObjConstruct(
+            CK.DB.Actor.Package actorPackage
+        )
+        {
+        }
+    }
+}
---
+++ run-cksetup.bat
@@ -0,0 +1,3 @@
+dotnet publish Bug_CKSetup_netcoreapp22_SCD -r win10-x64 -o publish-win10-x64
+
+cksetup run -v Debug -l cksetup.log cksetup.xml
